7f12bbea28ee8ca2b80f66c70de6f20cec469260
Jose Sissa <sissamendoza@outlook.com>
2024-11-04 15:51:58 -0500

    Selector of language

---
+++ src/i18n/ui.ts
@@ -0,0 +1,87 @@
+import SpainFlag from "@/components/flags/Spain.astro";
+import AndorraFlag from "@/components/flags/Andorra.astro";
+import USAFlag from "@/components/flags/USA.astro";
+
+export const languages: Record<
+    string,
+    { code: string; name: string; flag: typeof SpainFlag }
+> = {
+    ca: {
+        code: "ca",
+        name: "Català",
+        flag: AndorraFlag,
+    },
+    en: {
+        code: "en",
+        name: "English",
+        flag: USAFlag,
+    },
+    es: {
+        code: "es",
+        name: "Español",
+        flag: SpainFlag,
+    },
+};
+
+export const ui = {
+    es: {
+        "nav.inicio": "/",
+        "nav.vota": "vota",
+        "nav.info": "información",
+        "nav.archivo": "archivo",
+        "nav.legal": "aviso-Legal",
+        "nav.privacidad": "privacidad",
+        "nav.cookies": "cookies",
+    },
+    en: {
+        "nav.inicio": "/en/",
+        "nav.vota": "vote",
+        "nav.info": "information",
+        "nav.archivo": "archive",
+        "nav.legal": "legal-notice",
+        "nav.privacidad": "privacy",
+        "nav.cookies": "cookies",
+    },
+    ca: {
+        "nav.inicio": "/ca/",
+        "nav.vota": "vota",
+        "nav.info": "informació",
+        "nav.archivo": "arxiu",
+        "nav.legal": "avís-legal",
+        "nav.privacidad": "privacitat",
+        "nav.cookies": "cookies",
+    },
+} as const;
+
+export const routes = {
+    es: {
+        home: "nav.inicio",
+        vota: "nav.vota",
+        informacion: "nav.info",
+        archivo: "nav.archivo",
+        "aviso-legal": "nav.legal",
+        privacidad: "nav.privacidad",
+        cookies: "nav.cookies",
+    },
+    en: {
+        home: "nav.inicio",
+        vote: "nav.vota",
+        information: "nav.info",
+        archive: "nav.archivo",
+        "legal-notice": "nav.legal",
+        privacy: "nav.privacidad",
+        cookies: "nav.cookies",
+    },
+    ca: {
+        home: "nav.inicio",
+        vota: "nav.vota",
+        informacio: "nav.info",
+        arxiu: "nav.archivo",
+        "avis-legal": "nav.legal",
+        privacitat: "nav.privacidad",
+        cookies: "nav.cookies",
+    },
+} as const;
+
+export const showDefaultLang = false;
+export const defaultLang = "es";
---
+++ src/i18n/utils.ts
@@ -0,0 +1,39 @@
+import { ui, defaultLang, routes } from "./ui";
+import type { LanguageKeyUi, RouteKeyUi } from "@/types/types";
+
+export function getLangFromUrl(url: URL) {
+    const [, lang] = url.pathname.split("/");
+    if (lang in ui) return lang as keyof typeof ui;
+    return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+    return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+        return ui[lang][key] || ui[defaultLang][key];
+    };
+}
+
+function quitarTildes(texto: string) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+export function getRouteOfLanguage(lang: LanguageKeyUi, route: RouteKeyUi) {
+    return quitarTildes(ui[lang][route]);
+}
+
+export function translateRoute(
+    key: LanguageKeyUi,
+    idiom: LanguageKeyUi,
+    route
+) {
+    console.log(key, idiom, route);
+
+    const r = routes[idiom][route];
+    if (key == "es") {
+        const href = `/${ui[key][r]}`;
+        return quitarTildes(href);
+    } else {
+        const href = `/${key}/${ui[key][r]}`;
+        return quitarTildes(href);
+    }
+}
