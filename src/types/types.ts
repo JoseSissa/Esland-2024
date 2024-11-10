27116c99d2e38b8f76c85054b18c5bab35f824cb
Jose Sissa <sissamendoza@outlook.com>
2024-11-10 15:47:14 -0500

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,14 @@ import { ui, routes } from "@/i18n/ui";
 export type LanguageKeyUi = keyof typeof ui;
 export type RouteKeyUi = keyof (typeof ui)["es"];
 export type LanguageKeyRoutes = keyof typeof routes;
+export type CandidateType = {
+    id: string;
+    name: string;
+    image: string;
+    link?: string;
+}
+export type editionsVoteType = {
+    categoryName: string;
+    candidates: Array<CandidateType>;
+    id: string;
+}
