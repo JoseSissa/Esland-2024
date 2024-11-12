deb3503d49d35379cfd348aeea1a0de2a4bc69b6
Jose Sissa <sissamendoza@outlook.com>
2024-11-12 08:44:00 -0500

    System of votes from db

--- src/hooks/useVoteSystem.ts
+++ src/hooks/useVoteSystem.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "preact/hooks";
-import { MAX_CATEGORY } from "@/const/const";
-import { type typeVotesInLocal, type pageInfo, type typeVoteInDB } from "@/types/types";
-
+import { MAX_CATEGORY, MAX_VOTES_CATEGORY } from "@/const/const";
+import type { typeVotesInLocal, pageInfo } from "@/types/types";
+type typeVotes = Array<Array<string>>;
 
 export const useVoteSystem = () => {
     // Nos crea el estado que será un array con una logitud MAX_CATEGORY y donde cada elemento es un array vacío
@@ -33,19 +33,15 @@ export const useVoteSystem = () => {
     // Search info of votes in DB and update votes state
     useEffect(() => {
         async function fetchVotes() {
-            //  TODO: Decomment this section
-            // const response = await fetch("/api/getVotes");
-            // const dataFromDB = await response.json();
-            // let dataProcessed = [];
-            // for (let i = 0; i < dataFromDB.rows.length; i = i + MAX_VOTES_CATEGORY) {
-            //     const arr = dataFromDB.rows.slice(i, i + MAX_VOTES_CATEGORY);
-            //     dataProcessed.push(arr.map((elem: typeVote) => elem[3]));
-            // }
-            // localStorage.setItem("votesEsland", JSON.stringify(dataProcessed));
-            // setVotes(dataProcessed);
-
-            const newData = JSON.parse(localStorage.getItem("votesEsland"));
-            setVotes(newData);
+            const response = await fetch("/api/getVotes");
+            const dataFromDB = await response.json();
+            let dataProcessed = [];
+            for (let i = 0; i < dataFromDB.rows.length; i = i + MAX_VOTES_CATEGORY) {
+                const arr = dataFromDB.rows.slice(i, i + MAX_VOTES_CATEGORY);
+                dataProcessed.push(arr.map((elem: typeVotes) => elem[3]));
+            }
+            localStorage.setItem("votesEsland", JSON.stringify(dataProcessed));
+            setVotes(dataProcessed);
         }
         fetchVotes();
     }, []);
@@ -60,9 +56,21 @@ export const useVoteSystem = () => {
 
     // Handles pages navigarion
     const handlePagesNavigation = (categoryIndex: number) => {
+        if(categoryIndex == 12) {
+            votes.forEach(elem => {
+                if (elem.length !== 4) {
+                    alert('Debes votar a 4 candidatos por categoría')
+                    return;
+                }
+            })
+            setCategory(categoryIndex)
+            return;
+        }
+
         if (categoryIndex < 0) categoryIndex = MAX_CATEGORY - 1;
         else if (categoryIndex > MAX_CATEGORY - 1) categoryIndex = 0;
         setCategory(categoryIndex);
+        
     };
 
     return { votes, setVotes, pageInfo, category, setCategory, handlePagesNavigation };
